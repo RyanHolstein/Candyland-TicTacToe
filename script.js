a09b53b205cfd11c02dee0507c6cb5e0bb3dfcf6
Ryan Holstein <ryan.holstein@gmail.com>
2014-01-14 17:51:10 -0800

    candyland

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ function takeTurn() {
 	if (this.innerHTML == "&nbsp;" ) {
 		
 		if (playerTurn%2 == 0) {
-			this.innerHTML = "<img src='http://www.clker.com/cliparts/8/7/c/4/1224784889408279301bluefrog23_Peppermint_Candy.svg.thumb.png' />X";
+			this.innerHTML = "<img src='http://www.clker.com/cliparts/8/7/c/4/1224784889408279301bluefrog23_Peppermint_Candy.svg.thumb.png' />";
 			} else {
-			this.innerHTML = "O";
+			this.innerHTML = "<img src='http://static1.wikia.nocookie.net/__cb62488/clubpenguin/images/thumb/6/64/Gingerbread_Cookie_Costume_clothing_icon_ID_4473.png/100px-0,615,0,615-Gingerbread_Cookie_Costume_clothing_icon_ID_4473.png' />";
 			}
 
 			playerTurn++;
